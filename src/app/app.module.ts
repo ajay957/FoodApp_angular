63a48f5dfe0a00ee14cb15a1abdec1bca57fd4bd
Ajay957 <ajay74.aa82@gmail.com>
2022-11-29 07:44:59 +0530

    login commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 
 const myRoute:Routes=[
   {
     path:"",
     component:SignUpComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
